5fa605545cfc6d1bb57d811eb277f38eb9624577
Bruno Mecca <brunogmecca@gmail.com>
2018-02-22 07:03:26 +0100

    add

---
+++ test6.c
@@ -0,0 +1,22 @@
+#ifdef AAA
+int teste3 = 0;
+void aa(){
+
+}
+#endif
+
+#ifdef BBB
+int var = teste3;
+#endif
+
+#ifdef CCC
+void bb(){
+	aa();
+	teste3 = 30;
+	var = 30;
+}
+#endif
+
+int main(){
+	bb();	
+}
