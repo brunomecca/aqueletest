236ec8f6d3d86bd93b886bd3c9cf5ff324786e0e
Bruno Mecca <brunogmecca@gmail.com>
2018-03-06 13:32:58 -0400

    test

--- test6.c
+++ test6.c
@@ -1,17 +1,39 @@
+#ifdef AAA
+int teste3 = 0;
+void aa(){
+	int b = 30;
+	int xxaa = 100;
+	char o = 'b';
+}
+int hello = 10;
+#endif
+
 #ifdef BBB
 int ko = 104;
 int aaacx = 30;
 #endif
 
+int bb ((int));
+
+#ifdef CCC
+int bb(int xc){
+	aa();
+	teste3 = 30;
+	ko = 30 + aaacx;
+	int lop = 1;
+	return lop;
+}
+#endif
+
 #ifdef FUNC
 int cx(){
-	ko = 100;
+	ko = 100 + bb();
 	return 15;
 }
 #endif
 
 int main(){
-	bb();
+	bb(20);
 	testeTrueTrue = testeTrueTrue2;
 	char dd = 'd';
 }
