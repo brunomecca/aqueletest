ad612b110bd0557597c4deb94f3516997a933ce6
Bruno Mecca <brunogmecca@gmail.com>
2018-06-02 10:50:05 -0400

    ae

--- test6.c
+++ test6.c
@@ -1,30 +1,32 @@
 #ifdef AA
-int testeA = 10;
-
-int testeA2;
+int a = 100;
+int a2 = 100;
+int aa(){
+	return b;
+}
 #endif
 
 #ifdef BB
-int testeB;
-int testeB2 = testeA;
+int b = 10;
+int b2 = 100;
+void bb(){
+	return 10;
+}
 #endif
 
-int aa(){
-	return 10;
-}
-
-int aaTogether(){
-	return 10;
-}
-
-int arguTest(int a){
+#ifdef CC
+int cc(int a){
 	return a + 10;
 }
+#endif
 
 int main(){
-	int k, z, x, c, a, b, j, d;
-	aa(arguTest());
-	k = z = x = c = a = b = j = d = 1;
-	testeB = 1;
-	testeA = (testeB2 * 10 + (k*z + (x-c)))/testeB + aa(arguTest(aaTogether(aa(b)) + d) + b + d) + b + j + arguTest(10) + d;
+
+	cc(bb());
+	int zz = a + a2;
+	a2 = aa() + bb() + cc(b2);
+	b2 = a;
+	aa();
+	zz = cc(20);
+	return 0;
 }
